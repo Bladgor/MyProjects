892725444cf6b9f23d47c93b8392eee0c2dc48cd
Bladgor <vint_666@bk.ru>
2022-03-02 14:53:19 +0300

    Переделал 28-5-1

--- Practice/28. Углублённая работа с классами и встроенные декораторы/28 - 5 - 1. Работа с файлом.py
+++ Practice/28. Углублённая работа с классами и встроенные декораторы/28 - 5 - 1. Работа с файлом.py
@@ -9,24 +9,25 @@
 
 class File:
     def __init__(self, file: str, mode: str = None):
-        self.file = file
         self.mode = mode
+        if self.mode == 'r' or self.mode is None:
+            self.file = open(file, encoding='utf-8')
+        elif self.mode == 'w':
+            self.file = open(file, 'w', encoding='utf-8')
 
     def write(self, text):
         if self.mode == 'w':
             self.file.write(text)
+        else:
+            print('Файл не открыт в режиме записи.')
 
     def __enter__(self):
-        if self.mode == 'r' or self.mode is None:
-            self.file = open(self.file, encoding='utf-8')
-            return self.file
-        elif self.mode == 'w':
-            self.file = open(self.file, 'w', encoding='utf-8')
-            return self.file
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.file.close()
+        return True
 
 
-with File('example.txt', 'w') as file:
-    file.write('Всем привет!')
+with File('example.txt') as f:
+    f.write('Всем привет!')
