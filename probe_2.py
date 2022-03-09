cd1787b6f116ad957ae90cdeec612bceac302072
Vitaly Miroshnichenco <vint_666@bk.ru>
2022-03-09 17:52:33 +0300

    09.03.2022

--- probe_2.py
+++ probe_2.py
@@ -1,3 +1,5 @@
-x = 345
-print(28 % 8)
-
+with open('file.txt', encoding='utf-8') as f,\
+       open('new_file.txt', 'w', encoding='utf-8') as new_f:
+    for i in f:
+        string = i[10:14]
+        new_f.write(f'{string}\n')
