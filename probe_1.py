5e3343b9057c99835390036e3fe6d7dfc9ec18da
Vitaly Miroshnichenco <vint_666@bk.ru>
2022-05-04 17:48:39 +0300

    04.05.2022 - 2

--- probe_1.py
+++ probe_1.py
@@ -1,13 +1,50 @@
+# 1. Создать класс TrafficLight (светофор).
+# Определить у него один атрибут color (цвет) и метод running (запуск);
+# атрибут реализовать как приватный;
+# в рамках метода реализовать переключение светофора в режимы: красный, жёлтый, зелёный;
+# продолжительность первого состояния (красный) составляет 7 секунд,
+# второго (жёлтый) — 2 секунды, третьего (зелёный) — на ваше усмотрение;
+# переключение между режимами должно осуществляться только в указанном порядке (красный, жёлтый, зелёный);
+# проверить работу примера, создав экземпляр и вызвав описанный метод.
+# Задачу можно усложнить, реализовав проверку порядка режимов.
+# При его нарушении выводить соответствующее сообщение и завершать скрипт.
 
-n = 5
-k = 2
-index = 1
+from time import sleep
 
-num_list = [x for x in range(1, n + 1)]
-print(num_list)
-i = 0
-i = (i + k - 1) % (len(num_list))
-print(i)
-num_list.remove(i)
 
-print(num_list)
+class TrafficLight:
+    def __init__(self, color):
+        self.__color = color
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, color: str):
+        if color.lower() in ['красный', 'жёлтый', 'зелёный']:
+            self.__color = color.title()
+            print(True)
+        else:
+            raise ValueError('Недопустимое значение цвета')
+
+    def running(self):
+        print(self.__color)
+        for _ in range(10):
+            if self.__color == 'Красный':
+                sleep(7)
+                self.__color = 'Жёлтый'
+                print(self.__color)
+            elif self.__color == 'Жёлтый':
+                sleep(2)
+                self.__color = 'Зелёный'
+                print(self.__color)
+            elif self.__color == 'Зелёный':
+                sleep(5)
+                self.__color = 'Красный'
+                print(self.__color)
+
+
+traffic_light = TrafficLight('красный')
+traffic_light.running()
+
